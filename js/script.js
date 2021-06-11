4cd12d1f26b5202b5f2b9fb9ce0137ff36d30605
cel71 <celestialc71@libero.it>
2021-06-11 16:51:30 +0200

    js-campominato es5

--- js/script.js
+++ js/script.js
@@ -11,15 +11,54 @@ console.log(sixTeenRandomNumber);
 
 var user = [];
 var check = false;
-var tentativiUser = 5;
+var tentativiUser = "";
+var max = "";
+
+var livello = parseInt(prompt("CAMPO MINATO: Scegli il livello di difficoltà da 0 a 2"));
+while (livello !== 0 && livello !== 1 && livello !== 2) {
+    alert("ATTENZIONE SCEGLI LA DIFFICOLTA'");
+    livello = parseInt(prompt("Scegli il livello di difficoltà da 0 a 2"));
+}
+
+switch (livello) {
+    case 0:
+        max = 100;
+        tentativiUser = 84;
+        break;
+    case 1:
+        max = 80;
+        tentativiUser = 64;
+        break;    
+    case 2:
+        max = 50;
+        tentativiUser = 34;
+        break;
+}
 
 while (user.length < tentativiUser) {
-    var number = parseInt(prompt("BENVENUTO: Inserisci un numero tra 1 e 100"));
-    if (number < 1 || number > 100 || isNaN(number)) {
-        alert("I NUMERI INSERITI NON SONO COMPRESI TRA 1 E 100");
-        check = true;
-        break;
+    if (livello === 0){
+        var number = parseInt(prompt("BENVENUTO: Inserisci un numero tra 1 e 100"));
+        if (number < 1 || number > 100 || isNaN(number)) {
+            alert("I NUMERI INSERITI NON SONO COMPRESI TRA 1 E 100");
+            check = true;
+            break;
+        }
+    } else if (livello === 1){
+        var number = parseInt(prompt("BENVENUTO: Inserisci un numero tra 1 e 80"));
+        if (number < 1 || number > 80 || isNaN(number)) {
+            alert("I NUMERI INSERITI NON SONO COMPRESI TRA 1 E 80");
+            check = true;
+            break;
+        }
+    } else if (livello === 2){
+        var number = parseInt(prompt("BENVENUTO: Inserisci un numero tra 1 e 50"));
+        if (number < 1 || number > 50 || isNaN(number)) {
+            alert("I NUMERI INSERITI NON SONO COMPRESI TRA 1 E 50");
+            check = true;
+            break;
+        }
     }
+    
     if (!user.includes(number)) {
         if (sixTeenRandomNumber.includes(number)) {
             alert("HAI PRESO UNA MINA");
